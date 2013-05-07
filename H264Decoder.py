5575905e1196be249a0021a2db6d506126272202
shuffle2 <godisgovernment@gmail.com>
2013-05-07 09:42:15 -0700

    base dir, streams a/v decently...working on HID

---
+++ H264Decoder.py
@@ -0,0 +1,140 @@
+from cffi import FFI
+import pygame
+
+class H264Decoder:
+    def __init_ffi(s):
+        s.ffi = FFI()
+        s.ffi.cdef('''
+            // AVCODEC
+            
+            enum PixelFormat { PIX_FMT_RGBA, PIX_FMT_YUV420P, PIX_FMT_RGB24, ... };
+            
+            void avcodec_register_all(void);
+            
+            struct AVPacket { ...; uint8_t *data; int size; ...; };
+            void av_init_packet(struct AVPacket *pkt);
+            
+            enum CodecID { CODEC_ID_H264, ... };
+            struct AVCodec *avcodec_find_decoder(enum CodecID id);
+
+            struct AVCodecContext *avcodec_alloc_context3(struct AVCodec *codec);
+            
+            int avcodec_open2(struct AVCodecContext *avctx, struct AVCodec *codec,
+                            struct AVDictionary **options);
+            
+            struct AVFrame { uint8_t *data[4]; int linesize[4]; ...; int key_frame; ...; };
+            struct AVFrame *avcodec_alloc_frame(void);
+            
+            int avcodec_decode_video2(struct AVCodecContext *avctx, struct AVFrame *picture,
+                                    int *got_picture_ptr, struct AVPacket *avpkt);
+            
+            int avcodec_close(struct AVCodecContext *avctx);
+            
+            void av_free(void *ptr);
+            
+            int avpicture_get_size(enum PixelFormat pix_fmt, int width, int height);
+            
+            int avpicture_fill(struct AVPicture *picture, uint8_t *ptr,
+                            int pix_fmt, int width, int height);
+            
+            // SWSCALE
+            
+            #define SWS_FAST_BILINEAR ...
+            struct SwsContext *sws_getContext(int srcW, int srcH, enum PixelFormat srcFormat,
+                                            int dstW, int dstH, enum PixelFormat dstFormat,
+                                            int flags, struct SwsFilter *srcFilter,
+                                            struct SwsFilter *dstFilter, const double *param);
+            
+            int sws_scale(struct SwsContext *c, const uint8_t *const srcSlice[],
+                        const int srcStride[], int srcSliceY,
+                        int srcSliceH, uint8_t *const dst[],
+                        const int dstStride[]);
+            
+            void sws_freeContext(struct SwsContext *c);
+        ''')
+        s.ns = s.ffi.verify(source = '''
+            #include <libavcodec/avcodec.h>
+            #include <libswscale/swscale.h>
+            ''', libraries=['avcodec', 'swscale'])
+
+    def __init_avcodec(s):
+        s.ns.avcodec_register_all()
+
+        s.av_packet = s.ffi.new('struct AVPacket *')
+        s.ns.av_init_packet(s.av_packet)
+
+        s.codec = s.ns.avcodec_find_decoder(s.ns.CODEC_ID_H264)
+        if not s.codec:
+            raise Exception('avcodec_alloc_context3')
+        s.context = s.ns.avcodec_alloc_context3(s.codec)
+        if not s.context:
+            raise Exception('avcodec_alloc_context3')
+        if s.ns.avcodec_open2(s.context, s.codec, s.ffi.NULL) < 0:
+            raise Exception('avcodec_open2')
+        s.frame = s.ns.avcodec_alloc_frame()
+        if not s.frame:
+            raise Exception('avcodec_alloc_frame')
+        s.got_frame = s.ffi.new('int *')
+        s.out_frame = s.ns.avcodec_alloc_frame()
+
+    def __init__(s, (in_x, in_y), (out_x, out_y)):
+        s.sws_context = None
+        s.__init_ffi()
+        s.__init_avcodec()
+        s.update_dimensions((in_x, in_y), (out_x, out_y))
+
+    def close(s):
+        s.ns.sws_freeContext(s.sws_context)
+        s.ns.av_free(s.out_frame)
+        
+        s.ns.avcodec_close(s.context)
+        s.ns.av_free(s.context)
+        s.ns.av_free(s.frame)
+
+    def update_dimensions(s, (in_x, in_y), (out_x, out_y)):
+        s.in_x, s.in_y = in_x, in_y
+        s.out_x, s.out_y = out_x, out_y
+        
+        if s.sws_context != None:
+            s.ns.sws_freeContext(s.sws_context)
+        s.sws_context = s.ns.sws_getContext(
+            s.in_x, s.in_y, s.ns.PIX_FMT_YUV420P,
+            s.out_x, s.out_y, s.ns.PIX_FMT_RGB24,
+            s.ns.SWS_FAST_BILINEAR,
+            s.ffi.NULL,
+            s.ffi.NULL, s.ffi.NULL)
+        
+        bytes_req = s.ns.avpicture_get_size(s.ns.PIX_FMT_RGB24, s.out_x, s.out_y)
+        s.out_buffer = s.ffi.new('uint8_t [%i]' % bytes_req)
+        s.ns.avpicture_fill(
+            s.ffi.cast('struct AVPicture *', s.out_frame),
+            s.out_buffer,
+            s.ns.PIX_FMT_RGB24,
+            s.out_x, s.out_y)
+
+    def display_frame(s, encoded_nalu):
+        in_data = s.ffi.new('uint8_t []', encoded_nalu)
+        s.av_packet.data = in_data
+        s.av_packet.size = len(encoded_nalu)
+        
+        length = s.ns.avcodec_decode_video2(s.context, s.frame, s.got_frame, s.av_packet)
+        if length < 0:
+            raise Exception('avcodec_decode_video2')
+        elif length != s.av_packet.size:
+            raise Exception('expected to decode a single complete frame')
+        elif s.got_frame[0]:
+            #print 'keyframe:', s.frame.key_frame
+            # convert from YUV to RGB
+            out_height = s.ns.sws_scale(
+                s.sws_context,
+                s.frame.data, s.frame.linesize,
+                0, s.in_y,
+                s.out_frame.data, s.out_frame.linesize)
+            
+            #print out_height
+            surface = pygame.image.frombuffer(
+                s.ffi.buffer(s.out_frame.data[0], s.out_frame.linesize[0] * s.out_y),
+                (s.out_x, s.out_y),
+                'RGB')
+            pygame.display.get_surface().blit(surface, (0, 0))
+            pygame.display.flip()
---
+++ drc-sim.py
@@ -0,0 +1,349 @@
+import construct
+import pyaudio
+import select
+import socket
+import array
+import pygame
+import time
+from H264Decoder import H264Decoder
+
+pygame.init()
+pygame.display.set_mode([854, 480])
+pygame.display.set_caption("drc-sim")
+done = False
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+def service_addend(ip):
+    if int(ip.split('.')[3]) == 10:
+        return 0
+    else:
+        return 100
+
+def udp_service(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port + service_addend(ip)))
+    return sock
+
+PORT_MSG = 50010
+PORT_VID = 50020
+PORT_AUD = 50021
+PORT_HID = 50022
+PORT_CMD = 50023
+
+# hack for now, replace with dhcp result
+LOCAL_IP = '192.168.1.11'
+
+MSG_S = udp_service(LOCAL_IP, PORT_MSG)
+VID_S = udp_service(LOCAL_IP, PORT_VID)
+AUD_S = udp_service(LOCAL_IP, PORT_AUD)
+HID_S = udp_service(LOCAL_IP, PORT_HID)
+CMD_S = udp_service(LOCAL_IP, PORT_CMD)
+
+class ServiceBase(object):
+    def __init__(s):
+        s.seq_id_expect = None
+
+    def update_seq_id(s, seq_id):
+        ret = True
+        if s.seq_id_expect == None: s.seq_id_expect = seq_id
+        elif s.seq_id_expect != seq_id:
+            ret = False
+        s.seq_id_expect = (seq_id + 1) & 0x3ff
+        return ret
+
+pa_initted = False
+pa_num_bufs = 9
+pa_ring = [0] * pa_num_bufs
+pa_wpos = pa_rpos = 0
+def pa_callback(in_data, frame_count, time_info, status):
+    global pa_num_bufs, pa_ring, pa_wpos, pa_rpos
+    samples = pa_ring[pa_wpos]
+    pa_wpos += 1
+    pa_wpos %= pa_num_bufs
+    samples.extend(pa_ring[pa_wpos])
+    pa_wpos += 1
+    pa_wpos %= pa_num_bufs
+    return (samples, pyaudio.paContinue)
+
+class ServiceASTRM(ServiceBase):
+    def __init__(s):
+        super(ServiceASTRM, s).__init__()
+        s.header_base = construct.BitStruct('ASTRMBaseHeader',
+            construct.BitField('fmt', 3),
+            construct.Bit('channel'),
+            construct.Flag('vibrate'),
+            construct.Bit('packet_type'),
+            construct.BitField('seq_id', 10),
+            construct.BitField('payload_size', 16)
+        )
+        s.header_aud = construct.Struct('ASTRMAudioHeader',
+            construct.ULInt32('timestamp'),
+        #    construct.Array(lambda ctx: ctx.payload_size, construct.UBInt8("data"))
+        )
+        s.header_msg = construct.Struct('ASTRMMsgHeader',
+            # This is kind of a hack, (there are two timestamp fields, which one is used depends on packet_type
+            construct.ULInt32('timestamp_audio'),
+            construct.ULInt32('timestamp'),
+            construct.Array(2, construct.ULInt32('freq_0')), # -> mc_video
+            construct.Array(2, construct.ULInt32('freq_1')), # -> mc_sync
+            construct.ULInt8('vid_format'),
+            construct.Padding(3)
+        )
+        s.header = construct.Struct('ASTRMHeader',
+            construct.Embed(s.header_base),
+            construct.Switch('format_hdr', lambda ctx: ctx.packet_type,
+                {
+                    0 : construct.Embed(s.header_aud),
+                    1 : construct.Embed(s.header_msg),
+                },
+                default = construct.Pass
+            )
+        )
+
+    def update(s, packet):
+        global pa_ring, pa_initted, pa_rpos
+        h = s.header.parse(packet)
+        
+        # ignore vid_format packets for now
+        if h.packet_type == 0:
+            seq_ok = s.update_seq_id(h.seq_id)
+            if not seq_ok:
+                print 'astrm bad seq_id'
+            if h.fmt != 1 or h.channel != 0:
+                raise Exception('astrm currently only handles 48kHz PCM stereo')
+            if len(packet) != 8 + h.payload_size:
+                raise Exception('astrm bad payload_size')
+            
+            if h.vibrate:
+                print '*vibrate*'
+            
+            pa_ring[pa_rpos] = array.array('H', packet[8:])
+            pa_rpos += 1
+            pa_rpos %= pa_num_bufs
+            
+            if pa_rpos > 4 and pa_initted == False:
+                p = pyaudio.PyAudio()
+                stream = p.open(format = pyaudio.paInt16,
+                    channels = 2,
+                    rate = 48000,
+                    output = True,
+                    frames_per_buffer = 416 * 2,
+                    stream_callback = pa_callback)
+                pa_initted = True
+
+class ServiceVSTRM(ServiceBase):
+    dimensions = {
+        'camera' : (640, 480),
+        'gamepad' :  (854, 480)
+    }
+    
+    def __init__(s):
+        super(ServiceVSTRM, s).__init__()
+        s.decoder = H264Decoder(
+            s.dimensions['gamepad'],
+            pygame.display.get_surface().get_size())
+        s.header = construct.BitStruct('VSTRMHeader',
+            construct.Nibble('magic'),
+            construct.BitField('packet_type', 2),
+            construct.BitField('seq_id', 10),
+            construct.Flag('init'),
+            construct.Flag('frame_begin'),
+            construct.Flag('chunk_end'),
+            construct.Flag('frame_end'),
+            construct.Flag('has_timestamp'),
+            construct.BitField('payload_size', 11),
+            construct.BitField('timestamp', 32)
+        )
+        s.frame = array.array('B')
+        s.is_streaming = False
+        s.frame_decode_num = 0
+
+    def packet_is_idr(s, packet):
+        return packet[8:16].find('\x80') != -1
+
+    def h264_nal_encapsulate(s, is_idr, vstrm):
+        slice_header = 0x25b804ff if is_idr else (0x21e003ff | ((s.frame_decode_num & 0xff) << 13))
+        s.frame_decode_num += 1
+        
+        nals = array.array('B')
+        # TODO shouldn't really need this after the first IDR
+        # TODO hardcoded for gamepad for now
+        # allow decoder to know stream parameters
+        if is_idr:
+            nals.extend([
+                # sps
+                0x00, 0x00, 0x00, 0x01,
+                0x67, 0x64, 0x00, 0x20, 0xac, 0x2b, 0x40, 0x6c, 0x1e, 0xf3, 0x68,
+                # pps
+                0x00, 0x00, 0x00, 0x01,
+                0x68, 0xee, 0x06, 0x0c, 0xe8
+            ])
+        
+        # begin slice nalu
+        nals.extend([0x00, 0x00, 0x00, 0x01])
+        nals.extend([(slice_header >> 24) & 0xff,
+                     (slice_header >> 16) & 0xff,
+                     (slice_header >>  8) & 0xff,
+                      slice_header & 0xff])
+        
+        # add escape codes
+        nals.extend(vstrm[:2])
+        for i in xrange(2, len(vstrm)):
+            if vstrm[i] <= 3 and nals[-2] == 0 and nals[-1] == 0:
+                nals.extend([3])
+            nals.extend([vstrm[i]])
+        
+        return nals
+
+    def update(s, packet):
+        h = s.header.parse(packet)
+        is_idr = s.packet_is_idr(packet)
+        
+        seq_ok = s.update_seq_id(h.seq_id)
+        
+        if not seq_ok:
+            s.is_streaming = False
+        
+        if h.frame_begin:
+            s.frame = array.array('B')
+            if s.is_streaming == False:
+                if is_idr:
+                    s.is_streaming = True
+                else:
+                    # request a new IDR frame
+                    MSG_S.sendto('\1\0\0\0', ('192.168.1.10', PORT_MSG))
+                    return
+        
+        s.frame.fromstring(packet[16:])
+        
+        if s.is_streaming and h.frame_end:
+            nals = s.h264_nal_encapsulate(is_idr, s.frame)
+            s.decoder.display_frame(nals.tostring())
+
+class ServiceCMD(ServiceBase):
+    def update(s, packet):
+        print 'CMD', packet.encode('hex')
+
+class ServiceMSG(ServiceBase):
+    def update(s, packet):
+        print 'MSG', packet.encode('hex')
+
+class ServiceNOP(ServiceBase):
+    def update(s, packet):
+        pass
+
+service_handlers = {
+    MSG_S : ServiceMSG(),
+    VID_S : ServiceVSTRM(),
+    AUD_S : ServiceASTRM(),
+    CMD_S : ServiceCMD()
+}
+
+hid_seq_id = 0
+def hid_snd():
+    global joystick, hid_seq_id
+    
+    report = array.array('B', '\0' * 0x80)
+    
+    button_mapping = {
+        0 : 0x8000, # a
+        1 : 0x4000, # b
+        2 : 0x2000, # x
+        3 : 0x1000, # y
+        4 : 0x0020, # l
+        5 : 0x0010, # r
+        6 : 0x0004, # back (minus)
+        7 : 0x0008, # start (plus)
+        8 : 0x0002, # xbox (home)
+        # extra buttons
+        9 : 0x08, # l3
+       10 : 0x04  # r3
+    }
+    hat_mapping_x = {
+        0 : 0x000,
+       -1 : 0x800, # l
+        1 : 0x400, # r
+    }
+    hat_mapping_y = {
+        0 : 0x000,
+       -1 : 0x100, # d
+        1 : 0x200, # u
+    }
+    trigger_mapping = {
+        2 : 0x0080, # l
+        5 : 0x0040  # r
+    }
+    
+    # 16bit LE @ 0 seq_id
+    # seems to be ignored
+    report[1] = (hid_seq_id >> 8) & 0xff
+    report[0] = hid_seq_id & 0xff
+    # 16bit @ 2
+    button_bits = 0
+    for i in xrange(9):
+        if joystick.get_button(i):
+            button_bits |= button_mapping[i]
+    # hat: (<l/r>, <u/d>) [-1,1]
+    hat = joystick.get_hat(0)
+    button_bits |= hat_mapping_x[hat[0]]
+    button_bits |= hat_mapping_y[hat[1]]
+    # 16bit LE array @ 6
+    # LX, LY, RX, RY
+    # 0: l stick l/r
+    # 1: l stick u/d
+    # 2: l trigger
+    # 3: r stick l/r
+    # 4: r stick u/d
+    # 5: r trigger
+    def scale_stick(OldValue, OldMin, OldMax, NewMin, NewMax):
+        return int((((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin)
+    for i in xrange(6):
+        if i in (2, 5):
+            if joystick.get_axis(i) > 0:
+                button_bits |= trigger_mapping[i]
+        else:
+            orig = joystick.get_axis(i)
+            scaled = 0x800
+            if abs(orig) > 0.2:
+                if i in (0, 3):
+                    scaled = scale_stick(orig, -1, 1, 900, 3200)
+                elif i in (1, 4):
+                    scaled = scale_stick(orig, 1, -1, 900, 3200)
+            #print '%04i %04i %f' % (i, scaled, orig)
+            report[6 + i * 2 + 0] = scaled & 0xff
+            report[6 + i * 2 + 1] = (scaled >> 8) & 0xff
+    report[2] = (button_bits >> 8) & 0xff
+    report[3] = button_bits & 0xff
+    # 8bit @ 80
+    for i in xrange(9,11):
+        if joystick.get_button(i):
+            report[80] |= button_mapping[i]
+    
+    report[0x7f] = 0xe0
+    
+    HID_S.sendto(report, ('192.168.1.10', PORT_HID))
+    hid_seq_id += 1
+
+time.sleep(1)
+
+EVT_SEND_HID = pygame.USEREVENT
+pygame.time.set_timer(EVT_SEND_HID, int((1. / 180.) * 1000.))
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == EVT_SEND_HID:
+            hid_snd()
+    
+    rlist, wlist, xlist = select.select(service_handlers.keys(), (), (), 100)
+    
+    if not rlist:
+        continue
+    
+    for sock in rlist:
+        service_handlers[sock].update(sock.recvfrom(2048)[0])
+
+pygame.quit()
